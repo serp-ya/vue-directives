6dec62e0fe449b5908fb0c17dda5806aecfa6e3a
Sergei Kundriukov <Sergei_Kundriukov@epam.com>
2018-11-08 13:29:27 +0300

    Maked simple events handler directive

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+Vue.directive('event', {
+  bind(el, binding, VNode) {
+    const eventType = binding.arg;
+    const callbackName = binding.expression;
+    const callback = VNode.context[callbackName] || callbackName;
+
+    el.addEventListener(eventType, callback);
+  }
+});
 
 new Vue({
   el: '#app',
-  render: h => h(App)
-})
+  render: h => h(App),
+});
